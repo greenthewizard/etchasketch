e8561919fbcb65fe2041f49b653e8e60d93cd9ce
buddytherat <buddytherat@gmail.com>
2018-04-06 17:31:45 -0500

    Finished project

--- etch.js
+++ etch.js
@@ -1,26 +1,56 @@
 let $con = document.getElementById('etch-container');
-let etchWidth = 16;
-let etchHeight = 16;
-let boxSize = 48;
+let $btnClear = document.getElementById('btn-clear');
+let $btnResize = document.getElementById('btn-resize');
+let etchTiles = 16;
+let etchWidth = 768;
 
-$con.style.width = boxSize * etchWidth + "px";
-$con.style.height = boxSize * etchHeight + "px";
+$con.style.width = etchWidth + "px";
+$con.style.height = etchWidth + "px";
 
-for(let i = 0; i < etchWidth; i++) {
-	for(let j = 0; j < etchHeight; j++) {
-		let $newDiv = document.createElement('div');
+function populate() {
+	for(let i = 0; i < etchTiles; i++) {
+		for(let j = 0; j < etchTiles; j++) {
+			let $newDiv = document.createElement('div');
 
-		$newDiv.style.width = boxSize + "px";
-		$newDiv.style.height = boxSize + "px";
-		$newDiv.classList.add('etch-box');
+			$newDiv.style.width = etchWidth/etchTiles + "px";
+			$newDiv.style.height = etchWidth/etchTiles + "px";
+			$newDiv.classList.add('etch-box');
 
-		$con.appendChild($newDiv);
+			$con.appendChild($newDiv);
+		}
 	}
 }
 
 function fillSquare(e) {
-	console.log(e);
-	e.target.style.backgroundColor = 'black';
+	e.target.classList.add('filled');
 }
 
-$con.addEventListener('click', fillSquare);
+function clear() {
+	$boxes = document.querySelectorAll(".etch-box");
+	$boxes.forEach(function(box) {
+		box.classList.remove('filled');
+	});
+}
+
+function resize() {
+	let newTiles = prompt();
+	//Data Validation
+	newTiles = isPosInt(newTiles) ? newTiles : etchTiles;
+
+	//Clear boxes
+	while ($con.firstChild) {
+		$con.removeChild($con.firstChild);
+	}
+
+	etchTiles = newTiles;
+	populate();
+}
+
+function isPosInt(str) {
+	let n = Math.floor(Number(str));
+	return n !== Infinity && String(n) === str && n >= 0;
+}
+
+$con.addEventListener('mouseover', fillSquare);
+$btnClear.addEventListener('click', clear);
+$btnResize.addEventListener('click', resize);
